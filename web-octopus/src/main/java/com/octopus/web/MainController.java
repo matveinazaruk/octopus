ae1a0f3c9a097bcbd90736c7f7291dab37c75ff0
vildhet <vildhet@gmail.com>
2013-11-26 01:53:56 +0300

    added web

---
+++ web-octopus/src/main/java/com/octopus/web/MainController.java
@@ -0,0 +1,18 @@
+package com.octopus.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/graph")
+public class MainController {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String showGraph(ModelMap map) {
+		map.addAttribute("text", "Octopus");
+		return "graphics";
+	}
+
+}
